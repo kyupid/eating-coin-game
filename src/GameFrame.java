34a52fb8c5bc0c56638c759304f7e23cf4bf3747
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-30 14:26:48 +1300

    Set basic frame

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -3,9 +3,11 @@ import java.awt.*;
 class GameFrame extends Frame {
 
     GamePanel panel;
-    
 
     GameFrame() {
-
+        setTitle("Eat Coin");
+        setBounds(400, 200, 500, 500);
+        setResizable(false);
+        setVisible(true);
     }
  }
