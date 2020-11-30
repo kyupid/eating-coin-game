22da68bb7b5c2521db69a04c1623d12d9641dacc
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-11-30 14:32:18 +1300

    Add panel and set it to BorderLayout.CENTER

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -8,6 +8,11 @@ class GameFrame extends Frame {
         setTitle("Eat Coin");
         setBounds(400, 200, 500, 500);
         setResizable(false);
+
+        panel = new GamePanel();
+        add(panel, BorderLayout.CENTER);
+
+
         setVisible(true);
     }
  }
