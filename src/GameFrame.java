c622320d186f75bd5affa9196a6b86b0ecb4870e
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-01 15:20:14 +1300

    Create xPlayer and yPlayer to move imagePlayer.

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -4,7 +4,8 @@ import java.awt.event.*;
 
 class GameFrame extends JFrame implements KeyListener {
 
-    private boolean isKeyUp, isKeyDown, isKeyLeft, isKeyRight= false;
+    private boolean isKeyUp, isKeyDown, isKeyLeft, isKeyRight = false;
+    private boolean playerMove = false;
 
     private final int FRAME_WIDTH = 600;
     private final int FRAME_HEIGHT = 600;
@@ -29,37 +30,44 @@ class GameFrame extends JFrame implements KeyListener {
     private void init() {
         setTitle("Eat Coin");
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
+
+        //xPlayer, yPlayer를 프레임의 가운데에 배치하는 값을 저장한다.
+        xPlayer = (FRAME_WIDTH - imagePlayer.getWidth(this)) / 2;
+        yPlayer = (FRAME_HEIGHT - imagePlayer.getHeight(this)) / 2;
     }
 
-    public void paint (Graphics g) {
+    public void paint(Graphics g) {
+        //가상에 그려준다
+        bufferImage = createImage(FRAME_WIDTH, FRAME_HEIGHT);
+        gp = bufferImage.getGraphics();
+        gp.drawImage(imagePlayer, xPlayer, yPlayer, this);
 
-        g.drawImage(imagePlayer, 50, 50, this);
+
+        //가상에 그린 것을 딱! 한번에!
+        g.drawImage(bufferImage, 0, 0, this);
     }
 
-
+    private void move() {
+    }
 
     @Override
     public void keyPressed(KeyEvent e) {
 
-        switch(e.getKeyCode()) { //입럭된 키보드 값을 가져온다
+        switch (e.getKeyCode()) { //입럭된 키보드 값을 가져온다
             case KeyEvent.VK_UP:
                 isKeyUp = true;
-                yPlayer -= 5;
                 break;
 
             case KeyEvent.VK_DOWN:
                 isKeyDown = true;
-                yPlayer += 5;
                 break;
 
             case KeyEvent.VK_LEFT:
                 isKeyLeft = true;
-                xPlayer -= 5;
                 break;
 
             case KeyEvent.VK_RIGHT:
                 isKeyRight = true;
-                xPlayer += 5;
                 break;
         }
 
@@ -67,12 +75,26 @@ class GameFrame extends JFrame implements KeyListener {
 
     @Override
     public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()) { //입럭된 키보드 값을 가져온다
+            case KeyEvent.VK_UP:
+                isKeyUp = false;
+                break;
 
+            case KeyEvent.VK_DOWN:
+                isKeyDown = false;
+                break;
+
+            case KeyEvent.VK_LEFT:
+                isKeyLeft = false;
+                break;
+
+            case KeyEvent.VK_RIGHT:
+                isKeyRight = false;
+                break;
+        }
     }
-
     @Override
-    public void keyTyped(KeyEvent e) {
-
+    public void keyTyped (KeyEvent e){
     }
 }
 
