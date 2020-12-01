f2f3b6fca4e05f9180f258bc795b2eadd8e9397c
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-01 15:49:28 +1300

    Better moves for some reason

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -52,15 +52,18 @@ class GameFrame extends JFrame implements KeyListener, Runnable {
         yPlayer = (FRAME_HEIGHT - imagePlayer.getHeight(this)) / 2;
     }
 
+
+    public void update(Graphics g) {
+        g.drawImage(bufferImage, 0, 0, this);
+    }
+
     public void paint(Graphics g) {
         //가상에 그려준다
         bufferImage = createImage(FRAME_WIDTH, FRAME_HEIGHT);
         gp = bufferImage.getGraphics();
         gp.drawImage(imagePlayer, xPlayer, yPlayer, this);
 
-
-        //가상에 그린 것을 딱! 한번에!
-        g.drawImage(bufferImage, 0, 0, this);
+        update(g);
     }
 
     private void move() {
