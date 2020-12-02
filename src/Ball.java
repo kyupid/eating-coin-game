e96b8b26787bff6ac31812609bcb24a0f261645a
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-02 20:57:24 +1300

    Create class Ball to create obstacles

---
+++ src/Ball.java
@@ -0,0 +1,12 @@
+public class Ball {
+    int x, y;
+    static final int SIZE = 15;
+    int[] speed = {1, 2, 3, 4, 5};
+    int xStep, yStep;
+
+
+    Ball(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
