863d5dc2fc27d155826ba5e5726fef6a960a2d86
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-03 09:47:04 +1300

    Fixed ball's random speed that didn't work

--- src/Ball.java
+++ src/Ball.java
@@ -1,22 +1,23 @@
 import java.util.Random;
 
-public class Ball {
-    int x, y = 0;
+class Ball {
+    int x = 0;
+    int y = 0;
     static final int SIZE = 15;
     int[] speed = {1, 2, 3, 4, 5};
     int xStep, yStep;
 
     Random rd; // 무작위 speed를 위한
 
-    Ball() {
-    rd = new Random();
-    int s = rd.nextInt(5);
-    xStep = speed[s];
-    yStep = speed[s];
-    }
 
     Ball(int x, int y) {
         this.x = x;
         this.y = y;
+
+        rd = new Random();
+
+        int s = rd.nextInt(5);
+        xStep = speed[s];
+        yStep = speed[s];
     }
 }
