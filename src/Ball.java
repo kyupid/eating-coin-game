b635c123ddd2e744ef99507479a63c2a1fe0ec82
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-02 21:56:33 +1300

    Set ball's speed randomly

--- src/Ball.java
+++ src/Ball.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 public class Ball {
     int x, y = 0;
     static final int SIZE = 15;
     int[] speed = {1, 2, 3, 4, 5};
     int xStep, yStep;
 
+    Random rd; // 무작위 speed를 위한
+
+    Ball() {
+    rd = new Random();
+    int s = rd.nextInt(5);
+    xStep = speed[s];
+    yStep = speed[s];
+    }
 
     Ball(int x, int y) {
         this.x = x;
